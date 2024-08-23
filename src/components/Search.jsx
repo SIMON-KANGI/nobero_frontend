a3194852b050be8288a45fb366576293fdbb675c
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-23 17:32:46 +0300

    search

---
+++ src/components/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <div>
+    <form>
+        <div>
+            <input type="text" placeholder="Search..." className='border-2 w-38 p-1' />
+        </div>
+    </form>
+    </div>
+  )
+}
+
+export default Search
